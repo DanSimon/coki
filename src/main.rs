30a3ea861efc1710b43fd472af4f398bb21e1e8e
Dan Simon <dan@tumblr.com>
2014-10-31 22:11:32 -0400

    mostly working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![feature(globs)]
+#![feature(phase)]
+#![feature(unboxed_closures)]
 //extern crate regex_macros;
 //extern crate regex;
 //extern crate parsers;
@@ -13,7 +15,7 @@ pub mod parsers;
 fn main() {
 
   //let parser = CharP
-
+  /*
   let prog = vec![
     Assign("x", Num(3)), 
     Output(Variable("x")),
@@ -24,6 +26,7 @@ fn main() {
   ];
 
   run(&prog);
+  */
 
   let p2 = "x = 3 + 4";
   let t = tokenize(p2);
@@ -31,17 +34,63 @@ fn main() {
     println!("{}", x);
   }
 
-  let eq    = LiteralParser{literal: Equals};
-  let eq2   = LiteralParser{literal: Number(34)};
-  let por   = OrParser{a: &eq, b: &eq2};
-  let repEq = RepParser{reps: 3, parser: &por};
 
-
-  let data = [Equals, Equals, Number(34), PlusSign];
-  match repEq.parse(data) {
-    Ok(k) => {println!("ok");}
-    Err(err) => {println!("error: {}", err);}
+  /*
+  fn simplify<'a, I, O>(b: Box<Parser<'a, I, O> + 'a>) -> ParserGenerator<'a, I, O> {
+    box |&:| b
   }
+  */
+
+  /*
+  let rep_eq   = RepParser{
+    reps: 3,
+    parser: &OrParser{a: box |&:| box match_num as Box<Parser<&[Token], int>>, b: box |&:| box LiteralParser{literal: Equals} as Box<Parser<&[Token], Token>>}
+  };
+  */
+
+  fn expr<'a>() -> Box<Parser<'a, &'a [Token], Expr> + 'a> {
+
+    fn match_num<'a>() -> MatchParser<'a Token, Expr> {
+      MatchParser{matcher: box |&: input: &Token| -> Result<Expr, String> match *input {
+        Number(num) => Ok(Num(num)),
+        _ => Err(format!("wrong type"))
+      }}
+    }
+
+    let plus = |&:| {
+      MapParser{
+        parser: RepSepParser{
+          rep: match_num(),
+          sep: literal(PlusSign),
+          min_reps : 2
+        },
+        mapper: box |&: ops: Vec<Expr>| Plus(ops)
+      }
+    };
+
+    let simple_expr = OrParser{
+      b: box |&:| match_num() ,
+      a: box plus 
+    };
+
+    let mult = MapParser{
+      parser: RepSepParser{
+        rep: simple_expr,
+        sep: literal(MultSign),
+        min_reps : 2
+      },
+      mapper: box |&: ops: Vec<Expr>| Mult(ops)
+    };
+
+    box mult
+  }
+
+  let e = [Number(11), MultSign, Number(13), PlusSign, Number(14)];
+
+  let parser = expr();
+  println!("{}", parser.parse(&e));
+
+
 
 
 }
@@ -55,6 +104,7 @@ fn tokenize<'a>(input: &'a str) -> Vec<Token> {
       match tok.trim() {
         "=" => {build.push(Equals);}
         "+" => {build.push(PlusSign);}
+        "*" => {build.push(MultSign);}
         "out" => {build.push(OutputCmd)}
         "" => {}
         other => { build.push(Ident(String::from_str(other)))}
@@ -101,21 +151,44 @@ fn eval(expr: &Expr, env: &HashMap<&str, int>) -> Result<int, String> {
       None => Err(format!("Undefined var {}", var)),
     },
     Num(val) => Ok(val),
-    Plus(box ref a, box ref b) => match eval(a, env) {
-      Ok(va) => match eval(b, env) {
-        Ok(vb) => Ok(va + vb),
-        Err(err) => Err(err),
-      },
-      Err(err) => Err(err),
+    Plus(ref ops) => {
+      let mut sum = 0i;
+      for op in ops.iter() {
+        match eval(op, env) {
+          Ok(value) => {
+            sum += value;
+          }
+          Err(err) => {
+            return Err(err);
+          }
+        }
+      }
+      Ok(sum)
+    },
+    Mult(ref ops) => {
+      let mut total = 1i;
+      for op in ops.iter() {
+        match eval(op, env) {
+          Ok(value) => {
+            total += value;
+          }
+          Err(err) => {
+            return Err(err);
+          }
+        }
+      }
+      Ok(total)
     }
   }
 }
   
 
+#[deriving(Show)]
 enum Expr {
   Variable(&'static str),  
   Num(int),
-  Plus(Box<Expr>, Box<Expr>),
+  Plus(Vec<Expr>), //a + b + c + d
+  Mult(Vec<Expr>)
 }
 
 enum Statement {
@@ -132,6 +205,7 @@ enum Token {
   Ident(String),
   Number(int),
   PlusSign,
+  MultSign,
   OutputCmd,
   NewLine,
 }
--- src/parsers.rs
+++ src/parsers.rs
@@ -1,22 +1,37 @@
-#![feature(phase)]
-#[phase(plugin)]
 
-//use regex::{Captures, Regex};
+use std::ops::Fn;
 
-pub struct parser;
+pub fn literal<'a, T:'a + Eq>(literal: T) -> LiteralParser<'a, T> {
+  LiteralParser{literal: literal}
+}
+/*
+pub fn matcher<'a, I, O>(m: &'a Fn<(&'a I,), Result<O, String>>) -> MatchParser<'a, I, O> {
+  MatchParser{matcher: box *m}
+}
+*/
 
 pub type ParseResult<'a, I:'a, O> = Result<(O, I), String>;
 
 pub trait Parser<'a, I, O> {
 
   fn parse(&self, data: I) -> ParseResult<'a, I, O>;
+  /*
   fn and_then<B>(&'a self, next: &'a Parser<'a, I, B>) -> DualParser<'a, I, O ,B> {
     DualParser{first: self, second: next}
   }
+  */
+  /*
   fn or<B>(&'a self, next: &'a Parser<'a, I, B>) -> OrParser<'a, I, O, B> {
-    OrParser{a: self, b: next}
+    OrParser{a: box |&:| self, b: box |&:| next}
+  }*/
+  /*
+  fn map<B>(&'a self, f: &'a Fn<(O,), B>) -> MapParser<'a, I, O, B> {
+    MapParser{parser: self, mapper: f}
   }
+  */
 }
+
+
 
 trait TokenParser<'a, I, O> : Parser<'a, Vec<I>, O> {
 
@@ -28,6 +43,9 @@ pub struct LiteralParser<'a, T:'a + Eq> {
 
 impl<'a, T: 'a + Eq + Clone> Parser<'a,  &'a [T], T> for LiteralParser<'a, T> {
   fn parse(&self, data: &'a[T]) -> ParseResult<'a, &'a[T], T> {
+    if (data.len() < 1) {
+      return Err(format!("ran out of data"))
+    }
     if data[0] == self.literal {
       Ok((data[0].clone(), data.slice_from(1)))
     } else {
@@ -36,13 +54,20 @@ impl<'a, T: 'a + Eq + Clone> Parser<'a,  &'a [T], T> for LiteralParser<'a, T> {
   }
 }
 
-/*
 pub struct MatchParser<'a, I, O> {
-  matcher: |I| -> Option<O>
+  pub matcher: Box< Fn<(&'a I,), Result<O, String>> +'a>
 }
-impl
-*/
+impl<'a, I: Clone, O> Parser<'a, &'a [I], O> for MatchParser<'a, I, O> {
+  fn parse(&self, data: &'a[I]) -> ParseResult<'a, &'a[I], O> {
+    if (data.len() < 1) {
+      Err(format!("Unexpected End!"))
+    } else {
+      self.matcher.call((&data[0],)).map(|res| (res, data.slice_from(1)))
+    }
+  }
+}
 
+    
 
 pub struct RepParser<'a, I, O>{
   pub reps: uint,
@@ -68,13 +93,50 @@ impl<'a, I, O> Parser<'a, I, Vec<O>> for RepParser<'a, I, O> {
   }
 }
 
+//sep should return a Failure when it's time to stop repeating, the ok value is never used
+pub struct RepSepParser<'a, I, O, U, A: Parser<'a, I, O>, B: Parser<'a, I, U>> {
+  pub rep: A,
+  pub sep: B,
+  pub min_reps: uint,
+}
+impl<'a, I: Clone, O, U, A: Parser<'a, I, O>, B: Parser<'a, I, U>> Parser<'a, I, Vec<O>> for RepSepParser<'a, I, O, U, A, B> {
+  fn parse(&self, data: I) -> ParseResult<'a, I, Vec<O>> {
+    let mut remain = data;
+    let mut v: Vec<O> = Vec::new();    
+    loop {
+      match self.rep.parse(remain) {
+        Ok((result, rest)) => {
+          v.push(result);
+          match self.sep.parse(rest.clone()) {
+            Ok((_, rest2)) => {
+              remain = rest2
+            }
+            Err(_) => {
+              if (v.len() < self.min_reps) {
+                return Err(format!("Not enough reps: required {}, got {}", self.min_reps, v.len()))
+              } else {
+                return Ok((v, rest)) 
+              }
+            }
+          }
+        }
+        Err(err) => {
+          return Err(format!("Error on rep: {}", err));
+        }
+      }
+    }
+    //?
+  }
+}
+  
 
-pub struct DualParser<'a, I, A, B> {
-  first: &'a Parser<'a, I, A> + 'a,
-  second: &'a Parser<'a, I, B> + 'a
+
+pub struct DualParser<'a, I, A, B, X: Parser<'a, I, A>, Y: Parser<'a, I, B>> {
+  pub first: X,
+  pub second: Y
 }
 
-impl <'a, I, A, B> Parser<'a, I, (A,B)> for DualParser<'a, I, A, B> {
+impl <'a, I, A, B, X: Parser<'a, I, A>, Y: Parser<'a, I, B>> Parser<'a, I, (A,B)> for DualParser<'a, I, A, B, X, Y> {
   
   fn parse(&self, data: I) -> ParseResult<'a, I, (A, B)> {
     /*  doesn't work :(
@@ -94,26 +156,23 @@ impl <'a, I, A, B> Parser<'a, I, (A,B)> for DualParser<'a, I, A, B> {
   }
 }
 
-#[deriving(Show)]
-pub enum Or<A,B> {
-  OrA(A),
-  OrB(B),
-}
+//pub type ParserGenerator<'a, I , O> = Box<Fn<(), &'a Parser<'a, I, O> + 'a> + 'a>; //wat
+pub type ParserGenerator<'a, I , O> = Box<Fn<(), Box<Parser<'a, I, O> + 'a> + 'a> + 'a>; //wat
 
-pub struct OrParser<'a, I, A, B> {
-  pub a: &'a Parser<'a, I, A> + 'a,
-  pub b: &'a Parser<'a, I, B> + 'a
+pub struct OrParser<'a, I, O, A: Parser<'a, I, O>, B: Parser<'a, I, O>> {
+  pub a: Box<Fn<(), A> + 'a>,
+  pub b: Box<Fn<(), B> + 'a>
 }
 
 /*
  * Notice that I needs to be cloneable because we have to be able to hand it off to each parser
  */
-impl<'a, I: Clone, A, B> Parser<'a, I, Or<A,B>> for OrParser<'a, I, A, B> {
-  fn parse(&self, data: I) -> ParseResult<'a, I, Or<A, B>> {
-    match self.a.parse(data.clone()) {
-      Ok((a, d2)) => Ok((OrA(a), d2)),
-      Err(err) => match self.b.parse(data.clone()) {
-        Ok((b, remain)) => Ok((OrB(b), remain)),
+impl<'a, I: Clone, O, A: Parser<'a, I, O>, B: Parser<'a, I, O>> Parser<'a, I, O> for OrParser<'a, I, O, A, B> {
+  fn parse(&self, data: I) -> ParseResult<'a, I, O> {
+    match self.a.call(()).parse(data.clone()) {
+      Ok((a, d2)) => Ok((a, d2)),
+      Err(err) => match self.b.call(()).parse(data.clone()) {
+        Ok((b, remain)) => Ok((b, remain)),
         Err(err) => Err(err)
       }
     }
@@ -135,6 +194,17 @@ impl<'a, I: Clone, O> Parser<'a, I, O> for OneOfParser<'a, I, O> {
     Err(format!("All options failed"))
   }
 }
+
+pub struct MapParser<'a, I, O, U, A: Parser<'a, I, O>> {
+  pub parser: A,
+  pub mapper: Box<Fn<(O,), U> +'a>, //this has to be a &Fn and not a regular lambda since it must be immutable
+}
+impl<'a, I, O, U, A: Parser<'a, I, O>> Parser<'a, I, U> for MapParser<'a, I, O, U, A> {
+  fn parse(&self, data: I) -> ParseResult<'a, I, U> {
+    self.parser.parse(data).map(|(output, input)| ((self.mapper.call((output,)), input)))
+  }
+}
+
       
       
 
